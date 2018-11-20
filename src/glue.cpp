9e106a3cb05ad12dd9c0b48c702b073689f2e1a3
RugessNome <vincent.chambrin@gmail.com>
2018-11-20 18:47:58 +0100

    add LICENSE

--- src/glue.cpp
+++ src/glue.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2018 Vincent Chambrin
+// This file is part of the liblayout project
+// For conditions of distribution and use, see copyright notice in LICENSE
 
 #include "tex/glue.h"
 
